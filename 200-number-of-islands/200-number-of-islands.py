4a62f8890e6d2a94db04861e3a0c5f784c12507f
rch1025 <70617665+rch1025@users.noreply.github.com>
2022-09-19 21:37:04 +0900

    Time: 285 ms (97.99%), Space: 16.4 MB (81.11%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,5 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # DFS 함수 설정
         def dfs(i,j):
             # 인덱스가 범위 밖으로 나가거나 1이 아닌 경우 반환 설정
             if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] != "1":
